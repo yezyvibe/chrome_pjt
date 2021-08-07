29d41ed7de9ecf5b2a668fa08b3d8198788f2503
yezyvibe <yeji0501@gmail.com>
2021-08-07 21:36:14 +0900

    background & css layout

--- js/weather.js
+++ js/weather.js
@@ -8,8 +8,8 @@ function onGeoOk(position) {
     fetch(url)
         .then((response) => response.json())
         .then((data) =>{
-            const city = document.querySelector('#weather span:first-child');
-            const weather = document.querySelector('#weather span:last-child');
+            const city = document.querySelector('#weather h2:first-child');
+            const weather = document.querySelector('#weather h2:last-child');
             city.innerText = data.name;
             weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
         })
